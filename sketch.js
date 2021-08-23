11048b15248ddb685cc2753da3d36d2a6ae72efe
Artsy! <84583236+Artsy-Smartsy@users.noreply.github.com>
2021-08-23 21:46:15 +0530

    Added girl

--- sketch.js
+++ sketch.js
@@ -7,20 +7,26 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var backgroundImg;
 var hour, minute;
+var girlimg, girl;
 var frameRate;
 
 var bg = "sunrise1.png";
 
+
 function preload() {
     // create getBackgroundImg( ) here
+   girlimg=loadImage("girl.png")
+   
     getBackgroundImg();
 }
 
 function setup(){
     var canvas = createCanvas(1200,700);
+    girl= createSprite(600, 500, 150, 150);
+    girl.addImage(girlimg);
     engine = Engine.create();
     world = engine.world;
-
+    girl.scale=0.4;
 }
 
 function draw(){
@@ -32,31 +38,33 @@ function draw(){
     // write code to display time in correct format here
     fill("black");
     textSize(30);
-
-    if(hour>=12){
-        noStroke;
-        text("Time : "+ hour%12 + " PM", 50,100);
-       }else if(hour==0){
-        noStroke;
-         text("Time : 12 AM",100,100);
-       }else{
-        noStroke;
-        text("Time : "+ hour%12 + " AM", 50,100);
-       }
-         push();
-       strokeWeight(4);
+    strokeWeight(4);
        stroke(255);
   
+    if(hour>=12){
+
+        text("Time : "+ hour%12 + " PM", 50,100);
+       }else if(hour==0){
+ 
+         text("Time : 12 AM",100,100);
+       }else{
+   
+        text("Time : "+ hour%12 + " AM", 50,100);
+       }
+       push();
+   
     if(minute=minute) {  
-      text(""+hour +":"+minute, 1100,600)
+      text(""+hour +":"+minute, 1000,500);
+      text("The time is currently:",700, 500);
       pop();
     }
+    drawSprites();
 }
 
 async function getBackgroundImg(){
 
     // write code to fetch time from API
-    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/kolkata");
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
 
     //change the data in JSON format
     var responseJSON = await response.json();
